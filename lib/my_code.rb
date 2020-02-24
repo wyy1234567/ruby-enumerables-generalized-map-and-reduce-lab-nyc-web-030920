c51bd618f9064a146b97d73f0cbad7bec5e263ee
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-24 20:58:31 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(array) 
+  index = 0 
+  ans = []
+  while index < array.length do
+    ans.push(yield(array[index]))
+    index += 1 
+  end
+  ans
+end
+
+def reduce(array, starting = nil)
+  if starting
+    ans = starting
+    index = 0 
+  else
+    ans = array[0] 
+    index = 1 
+  end
+  while index < array.length do 
+    ans = yield(ans, array[index])
+    index += 1 
+  end
+  ans
+end
